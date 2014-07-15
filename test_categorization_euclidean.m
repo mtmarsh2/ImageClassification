45409239203332d7ed51d80f5a69e049974c2063
Michael Marshall <mm71593@vpn3-144203.near.uiuc.edu>
2014-07-15 00:03:52 -0400

    Added functionality to load sift descriptors from saved matrix

--- test_categorization_euclidean.m
+++ test_categorization_euclidean.m
@@ -1,4 +1,4 @@
-function [ predicted_category ] = test_categorization_euclidean( target_image_name, list_of_images, num_clusters, method )
+function [ predicted_category ] = test_categorization_euclidean( target_image_name, list_of_images, global_sift_descriptors, num_clusters, method )
 %Takes in target image, list of images and 
 
 %%
@@ -19,42 +19,43 @@ end
 %2. find features, descriptors in images and save them into global matrix
 %%
 
-global_sift_descriptors = [];
+
 global_image_histograms = zeros(num_images, num_clusters);
-image_sift_indexes = 1;
-for i=1:numel(list_of_images)
-	%image_sift_indexes = [image_sift_indexes size(global_sift_descriptors)];
-	image = imread(list_of_images{i});
-	if size(image,3) ~= 1
-		image = rgb2gray(image);
-	end
-	[f,d] = vl_sift(single(image));
-	%add descriptors to global matrix 
-	global_sift_descriptors = [global_sift_descriptors d(:,:)];
-end
 
 %3. use k means clustering on matrix
 global_sift_descriptors = double(global_sift_descriptors);
 [centers, assignments] = vl_kmeans(global_sift_descriptors, num_clusters);
-keyboard;
 
+%{
 %4. compute histogram vectors for each image
 for i=1:numel(list_of_images)
-    image = imread(list_of_images{i});
+    keyboard;
+    image = imread(list_of_images{i,1});
 	if(size(image,3) ~= 1)
 		image = rgb2gray(image);
 	end
 	[f,d] = vl_sift(single(image));
     [mins, indexs] = min( (vl_alldist(double(d), centers))');
     indexs = indexs';
-    keyboard;
     for j=1:size(indexs)
         global_image_histograms(i,indexs(j)) = global_image_histograms(i,indexs(j))  + 1;
     end
 end
-
+%}
 
 %5. compute histogram on target_image
+current_assignment_index = 1;
+for i=1:num_images
+    %get number of sift descriptors for current image
+    num_of_descriptors = size(list_of_images{i,2});
+    current_image_sif_descriptors = list_of_images{i,2};
+    [mins, indexs] = min( (vl_alldist(double(current_image_sif_descriptors), centers))');
+    current_assignment_index = current_assignment_index + num_of_descriptors;
+    indexs = indexs';
+    for j=1:size(indexs)
+        global_image_histograms(i,indexs(j)) = global_image_histograms(i,indexs(j))  + 1;
+    end
+end
 
 target_image_histogram = zeros(1, num_clusters);
 [tf, td] = vl_sift(single(target_image));
--- test_euclidean.m
+++ test_euclidean.m
@@ -5,7 +5,9 @@ directory_2 = 'okapi/';
 directory_3 = 'crocodile/';
 category_list = {directory_1; directory_2; directory_3};
 num_directories = size(category_list);
+generate_sift = 1;
 
+global_sift_descriptors = [];
 %get all relevant files
 for d=1:num_directories
     filelist = dir( [ category_list{d} '*.jpg' ] );
@@ -13,13 +15,32 @@ for d=1:num_directories
     tempfilelist = cell(size(filelist, 1), 1);
     %append category to filename for every file in folder
     for i=1:size(filelist)
-        image_name = strcat(category_list{d}, filelist(i).name);
-        tempfilelist{i} = image_name;
+        full_image_name = strcat(category_list{d}, filelist(i).name);
+        tempfilelist{i,1} = full_image_name;
+        
+        if generate_sift
+            image = imread(full_image_name);
+            if size(image,3) ~= 1
+                image = rgb2gray(image);
+            end
+            [f,sift_descriptors] = vl_sift(single(image));
+            %[~, short_name, ~] = fileparts(full_image_name);
+            save( strcat(full_image_name, '.mat'), 'sift_descriptors');
+            
+        else
+            sift_mat = load( strcat(full_image_name, '.mat'), 'sift_descriptors');
+            sift_descriptors = sift_mat.sift_descriptors;
+        end
+        %associate image's sift descriptors to its name
+        tempfilelist{i,2} = sift_descriptors;
+        
+        %add image's sift descriptors to global total matrix
+        global_sift_descriptors = [global_sift_descriptors sift_descriptors(:,:)];
     end
+    
     if ~exist('fullfilelist', 'var')
         fullfilelist = [tempfilelist];
     else
-    
     fullfilelist = [fullfilelist; tempfilelist];
     end
 end
@@ -45,15 +66,17 @@ for i=min_num_clusters:5:max_num_clusters
         current_image = current_image{1};
         correct_category = strsplit(current_image, '/');
         correct_category = correct_category(1);
-        if strcmp(correct_category, test_categorization_euclidean(current_image, fullfilelist, i,  'top_by_first_image'))
+        
+        if strcmp(correct_category, test_categorization_euclidean(current_image, fullfilelist, global_sift_descriptors, i,  'top_by_first_image'))
             num_right = num_right + 1;
         end
-        if strcmp(correct_category, test_categorization_euclidean(current_image, fullfilelist, i,  'top_by_sim'))
+        if strcmp(correct_category, test_categorization_euclidean(current_image, fullfilelist, global_sift_descriptors, i,  'top_by_sim'))
             num_right = num_right + 1;
         end
-        if strcmp(correct_category, test_categorization_euclidean(current_image, fullfilelist, i, 'top_by_count'))
+        if strcmp(correct_category, test_categorization_euclidean(current_image, fullfilelist, global_sift_descriptors, i, 'top_by_count'))
             num_right = num_right + 1;
         end
+        
         j
     end
     clusters_map{i/5,1} = i;
